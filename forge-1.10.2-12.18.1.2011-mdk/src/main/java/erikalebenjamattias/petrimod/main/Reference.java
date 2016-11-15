7bc8c2175f657cb34a49c1a25e2baad0940f2817
Mattias McMullin <mathewmcmullin@gmail.com>
2016-11-15 09:58:59 +0100

    Renamed Things to be More Petrified

---
+++ forge-1.10.2-12.18.1.2011-mdk/src/main/java/erikalebenjamattias/petrimod/main/Reference.java
@@ -0,0 +1,8 @@
+package erikalebenjamattias.petrimod.main;
+
+public class Reference {
+
+	public static final String MOD_ID = "petrimod";
+	public static final String MOD_NAME = "Petri Mod";
+	public static final String VERSION = "1.0";
+}
